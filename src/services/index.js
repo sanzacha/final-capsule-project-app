68fb46cacc26e92f5a090b64b741b5b72f198fda
Santhosh Zacharia <szacharia@sapient.com>
2018-09-13 10:05:37 +0530

    Build issue fixed

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,22 @@
 import axios from 'axios';
 
+// export function getUserName(username) {
+//   return axios
+//     .post('http://localhost:3001/users', { username })
+//     .then(
+//       result =>
+//         new Promise((resolve, reject) => {
+//           resolve(username);
+//         }),
+//     )
+//     .catch(error => {
+//       return username;
+//     });
+// }
+
 export function getUserName(username) {
   return axios
-    .post('http://localhost:3001/users', { username })
+    .post('/users', { username })
     .then(
       result =>
         new Promise((resolve, reject) => {
